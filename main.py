36d8ac86cecedd144717891c5f05f1a6bd9d5291
Toby Rea <toby.e.rea@protonmail.com>
2023-01-04 14:23:17 +0000

    Refactor configuration options
    - Moved them back into the file for now
    - Removed TQDM dependency (unnecessary requirement)

--- main.py
+++ main.py
@@ -13,29 +13,49 @@ import requests
 from selectolax.parser import HTMLParser
 import pandas as pd
 from pathlib import Path
-import tomllib
-from tqdm import tqdm
+
+
+URL = "https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx?Component="
+DATA_DIRECTORY = "data"
+COMPONENTS = [
+    "Demographics",
+    "Dietary",
+    "Examination",
+    "Laboratory",
+    "Questionnaire"
+]
+YEARS = [
+    "1999-2000",
+    "2001-2002",
+    "2003-2004",
+    "2005-2006",
+    "2007-2008",
+    "2009-2010",
+    "2011-2012",
+    "2013-2014",
+    "2015-2016",
+    "2017-2018",
+    "2017-2020",
+    "2019-2020",
+    "2021-2022"
+]
+
 
 def scrape_datasets() -> pd.DataFrame:
     """ Scrapes all publicly available NHANES datasets. """
-
-    with open("settings.toml", "rb") as config_file:
-        config = tomllib.load(config_file)
-        components = config["components"]
-        url = config["url"]
 
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 \
     (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"}
 
     df = pd.DataFrame(columns=["years", "component", "docs_url", "data_url"])
     base = "https://wwwn.cdc.gov"
-    for component in components:
-        new_url = url + component
+    for component in COMPONENTS:
+        new_url = URL + component
         response = requests.get(new_url, headers=headers)
         tree = HTMLParser(response.text)
         selector = "table > tbody > tr"
 
-        for node in tqdm(tree.css(selector), bar_format="{desc:25}|{bar:25}{r_bar}", desc=f"Component: {component}"):
+        for node in tree.css(selector):
             if "limited_access" in node.html.lower():
                 continue
             if "withdrawn" in node.html.lower():
@@ -55,11 +75,11 @@ def scrape_datasets() -> pd.DataFrame:
     return df
 
 
-def download_file(url: str, destination: Path) -> None:
+def download_file(url: str) -> None:
     """ Downloads a file from a url, if it doesn't already exist. """
 
     file_name = url.split("/")[-1]
-    file_path = destination / file_name
+    file_path = Path(DATA_DIRECTORY) / file_name
     if file_path.exists():
         print(f"Skipping {file_name} ... File already exists.")
         return
@@ -75,17 +95,13 @@ def download_nhanes(components: list[str] | None = None,
                     include_docs: bool = False) -> None:
     """ Downloads datasets and optionally documentation from NHANES. """
 
-    with open("settings.toml", "rb") as config:
-        destination = Path(tomllib.load(config)["data_directory"])
-        if not destination.is_dir():
-            Path.mkdir(destination)
+    if not Path(DATA_DIRECTORY).is_dir():
+        Path.mkdir(destination)
 
     if components is None:
-        with open("settings.toml", "rb") as config:
-            components = tomllib.load(config)["components"]
+        components = COMPONENTS
     if years is None:
-        with open("settings.toml", "rb") as config:
-            years = tomllib.load(config)["years"]
+        years = YEARS
 
     # Create the available nhanes dataset if it doesn't exist
     if not Path("nhanes_datasets.csv").is_file():
@@ -97,15 +113,15 @@ def download_nhanes(components: list[str] | None = None,
 
     # Filter and download
     df = df[(df["years"].isin(years)) & (df["component"].isin(components))]
-    [download_file(data, destination) for data in df["data_url"]]
+    [download_file(data) for data in df["data_url"]]
     if include_docs:
-        [download_file(doc, destination) for doc in df["docs_url"]]
+        [download_file(doc) for doc in df["docs_url"]]
 
 
 def main() -> None:
     # Default behaviour is to download all components over all years
     # excluding documentation. You can override this behaviour by
-    # passing options here or overriding 'settings.toml'
+    # passing options here
     download_nhanes(
         components=["Laboratory"],
         years=["2013-2014"],
