41fbe76b7cfd90ead8d0d6abe069832b54c34921
Toby Rea <toby.e.rea@protonmail.com>
2023-04-04 23:32:41 +0100

    Improved description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='nhanes_utils',
     version='1.0.0',
-    description='A package that can be used to download and process data from the NHANES.',
+    description='Utilities for working with NHANES data',
     author='Toby Rea',
     author_email='toby.e.rea@protonmail.com',
     url='https://github.com/toby-rea/nhanes_utils',
