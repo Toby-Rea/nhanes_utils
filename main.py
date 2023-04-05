f85b70475982ad7d6c813a3be7bebf055d7e5e49
Toby Rea <toby.e.rea@protonmail.com>
2023-04-05 14:27:09 +0100

    Improve commenting conciseness

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main() -> None:
     # Default behaviour is to download all known components over all known years, excluding documentation.
     #
     # You can override this behaviour by passing specific options here. For example, to download only the Laboratory
-    # datasets for 2017-2018, including documentation:
+    # datasets for 2017-2018, including documentation, to the "datasets" directory:
     download_nhanes(components=["Laboratory"], years=["2011-2012"], include_docs=False, destination="datasets")
 
     # Optionally, convert all the XPT files to CSV, a human-readable format
