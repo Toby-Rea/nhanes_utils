209a1449715e3dfafb9d91868a9f67a40de66b9e
Toby Rea <toby.e.rea@protonmail.com>
2023-04-05 16:38:16 +0100

    Ensure file writing is handled asynchronously. Introduce rate limiting to prevent flooding the server

--- nhanes_utils/downloader.py
+++ nhanes_utils/downloader.py
@@ -8,7 +8,15 @@ Toby Rea
 import asyncio
 from pathlib import Path
 
+import aiofiles
 import aiohttp
+
+
+async def _write_file(path: Path, content: bytes) -> None:
+    """ Writes the file content to disk. """
+
+    async with aiofiles.open(path, "wb") as file:
+        await file.write(content)
 
 
 class Downloader:
@@ -16,45 +24,41 @@ class Downloader:
         self.url_list: list[str] = url_list
         self.destination: str = destination
 
-    async def download_file(self, url: str) -> None:
-        """ Downloads a file from a given url, if it doesn't already exist. """
+    async def download_file(self, url: str, throttler) -> None:
+        """Downloads a file from a given url, if it doesn't already exist."""
 
-        # Get the file name and extension from the url, and convert the file extension to lowercase.
-        file_name = url.split("/")[-1]
-        extension = file_name.split(".")[-1]
-        file_name = file_name.replace(extension, extension.lower())
+        async with throttler:
+            # Get the file name and extension from the url, and convert the file extension to lowercase.
+            file_name = url.split("/")[-1]
+            extension = file_name.split(".")[-1]
+            file_name = file_name.replace(extension, extension.lower())
 
-        # Ensure the file doesn't already exist.
-        exists: bool = False
-        path = Path(self.destination).joinpath(file_name)
-        match extension.lower():
-            case "xpt" | "csv":
-                exists = path.with_suffix(".xpt").exists() or path.with_suffix(".csv").exists()
-            case _:
-                exists = path.exists()
+            # Ensure the file doesn't already exist.
+            exists = False
+            path = Path(self.destination).joinpath(file_name)
+            match extension.lower():
+                case "xpt" | "csv":
+                    exists = path.with_suffix(".xpt").exists() or path.with_suffix(".csv").exists()
+                case _:
+                    exists = path.exists()
 
-        if exists:
-            return
+            if exists:
+                return
 
-        async with aiohttp.ClientSession() as session, session.get(url) as response:
-            if response.status == 200:
-                content = await response.read()
-                with open(path, "wb") as file:
-                    file.write(content)
-            else:
-                print(f"Failed to download from {url} (received response code {response.status}).")
+            async with aiohttp.ClientSession() as session, session.get(url) as response:
+                if response.status == 200:
+                    content = await response.read()
+                    await _write_file(path, content)
+                else:
+                    print(f"Failed to download from {url} (received response code {response.status}).")
 
     async def download(self) -> None:
         """ Downloads all files stored in the url list. """
 
+        throttler = asyncio.Semaphore(10)
         print(f"Downloading files to {self.destination}...")
-        tasks = []
-        for url in self.url_list:
-            task = asyncio.create_task(self.download_file(url))
-            tasks.append(task)
-
+        tasks = [asyncio.create_task(self.download_file(url, throttler=throttler)) for url in self.url_list]
         await asyncio.gather(*tasks)
-
         print("Downloading complete!")
 
     def run(self) -> None:
