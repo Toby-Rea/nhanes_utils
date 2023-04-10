d2460ce2c6f509d6c9d389de56d44db7a221c9cb
Toby Rea <toby.e.rea@protonmail.com>
2023-04-10 22:45:58 +0100

    Huge overhaul. Fixes downloading for large files, hopefully performance gains too with improved logic and scraping

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from nhanes_utils import download_nhanes, convert_datasets
 def main() -> None:
     # Default behaviour is to download all known components over all known years, excluding documentation.
     #
-    # You can override this behaviour by passing specific options here. For example, to download only the Laboratory
-    # datasets for 2017-2018, including documentation, to the "datasets" directory:
-    download_nhanes(components=["Laboratory"], years=["2011-2012"], include_docs=False, destination="datasets")
+    # You can override this behaviour by passing specific options here...
+    download_nhanes()
 
     # Optionally, convert all the XPT files to CSV, a human-readable format
-    convert_datasets(data_directory="datasets")
+    convert_datasets()
 
 
 if __name__ == "__main__":
--- nhanes_utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from .nhanes_utils import get_available_datasets, download_nhanes, convert_datasets
